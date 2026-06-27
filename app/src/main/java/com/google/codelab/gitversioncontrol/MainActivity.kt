9c56efed18a9ef7552abbe25d73e517e1e5471b8
Gaurav singh <alexgaurav29@gmail.com>
2026-06-27 10:38:20 +0530

    Revert "This is our third commit"
    This reverts commit 7be05233cc8b9b6cbc2ef9d0a5274280abec831b.

--- app/src/main/java/com/google/codelab/gitversioncontrol/MainActivity.kt
+++ app/src/main/java/com/google/codelab/gitversioncontrol/MainActivity.kt
@@ -27,8 +27,6 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                     println("This is the second commit")
-                    this is a good day
-
                 }
             }
         }
